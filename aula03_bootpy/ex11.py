c8ae5766f471b8523f2f65b78757a8a075cbef6a
argus portal <argusportal@gmail.com>
2024-04-29 20:29:06 -0300

    atualizando ex 11

--- aula03_bootpy/ex11.py
+++ aula03_bootpy/ex11.py
@@ -1,2 +1,9 @@
 ### Exercício 11. Leitura de Dados até Flag
 # Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+dados = []
+entrada = ""
+while entrada.lower() != "sair":
+    entrada = input("Digite um valor (ou 'sair' para terminar): ")
+    if entrada.lower() != "sair":
+        print("Saindo")
