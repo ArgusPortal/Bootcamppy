530f3775aea059e493ab4f78525ef81c26f1bf68
argus portal <argusportal@gmail.com>
2024-05-01 12:47:00 -0300

    exercicio 9

--- aula2_bootpy/exercicio9.py
+++ aula2_bootpy/exercicio9.py
@@ -1,2 +1,6 @@
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
+# celsius = float(input("Digite a temperatura em Celsius: "))
+celsius = 30.0  # Exemplo de entrada
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C é igual a {fahrenheit}°F")
