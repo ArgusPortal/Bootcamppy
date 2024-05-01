170d2f3375a7ace7747ffd1f35ae6a4b29c65642
argus portal <argusportal@gmail.com>
2024-05-01 12:46:22 -0300

    exercicio 8

--- aula2_bootpy/exercicio8.py
+++ aula2_bootpy/exercicio8.py
@@ -1,2 +1,8 @@
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 
+# base = float(input("Digite a base: "))
+# expoente = float(input("Digite o expoente: "))
+base = 2.0  # Exemplo de entrada
+expoente = 3.0  # Exemplo de entrada
+potencia = base ** expoente
+print("O resultado da potência é:", potencia)
