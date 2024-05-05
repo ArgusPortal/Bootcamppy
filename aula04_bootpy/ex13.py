cd61adc97a45391917bc2203fe63d68b26d941e1
argus portal <argusportal@gmail.com>
2024-05-05 17:43:31 -0300

    exercicio 13

---
+++ aula04_bootpy/ex13.py
@@ -0,0 +1,8 @@
+## 13. Filtragem de Dados em Dicionário
+## Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+
+estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
+
+estoque_positivo = {produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
+
+print(estoque_positivo)
