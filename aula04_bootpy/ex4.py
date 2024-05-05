2c58854161e815b3deeb872d9a7b54a222c8c34b
argus portal <argusportal@gmail.com>
2024-05-05 17:34:01 -0300

    exercicio 4

---
+++ aula04_bootpy/ex4.py
@@ -0,0 +1,10 @@
+#   4. Contar ocorrências de caracteres
+
+
+def contar_caracteres(s):
+    contagem = {}
+    for caractere in s:
+        contagem[caractere] = contagem.get(caractere, 0) + 1
+    return contagem
+
+print(contar_caracteres("engenharia de dados"))
